7501c082c040070e3e0e350a0a1a83da49a5ef25
Julianna Cole <jcole@accme.org>
2021-04-20 16:17:01 -0500

    added wamember route

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, make_response, jsonify
+import json
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    print (request.is_json)
-    content = request.get_json()
-    print (content)
-    return (content)
-    #return 'Hello WOrld JSON posted'
+   return "Hello World JSON posted"
   
+@app.route("/wamember", methods=['GET', 'POST'])
+def wamember():
+    #print (request.is_json)
+    content = request.get_json()
+    #print (content)
+    return json.dumps(content)
+    #return ("Hello World - in wamember")
