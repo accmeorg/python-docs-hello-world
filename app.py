656c17473ef60017636bffc30a98b407c19a14a7
Julianna Cole <jcole@accme.org>
2021-04-21 09:21:01 -0500

    updated wamember to dump queue

--- app.py
+++ app.py
@@ -1,16 +1,66 @@
 from flask import Flask, request, make_response, jsonify
 import json
+import redis
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
    return "Hello World JSON posted"
-  
+
 @app.route("/wamember", methods=['GET', 'POST'])
 def wamember():
+    #set up redis cache access
+    myHostname = "poc-a2subcache.redis.cache.windows.net"
+    myPassword = "NJmqGdGgab4ZVlMcGKPbdF48D9JJilL+tExVYI+PU9Y="
+    r = redis.StrictRedis(host=myHostname, port=6380,
+                          password=myPassword, ssl=True)
+
     #print (request.is_json)
     content = request.get_json()
+    result = r.set(content['AccountId'],json.dumps(content))
+    print (content['AccountId'])
     #print (content)
     return json.dumps(content)
     #return ("Hello World - in wamember")
+
+
+@app.route("/a2queue", methods=['GET', 'POST'])
+def a2queue():
+    myHostname = "poc-a2subcache.redis.cache.windows.net"
+    myPassword = "NJmqGdGgab4ZVlMcGKPbdF48D9JJilL+tExVYI+PU9Y="
+
+    r = redis.StrictRedis(host=myHostname, port=6380,
+                          password=myPassword, ssl=True)
+
+    mymap = r.keys(pattern='*')
+    for key in mymap:
+        value = r.get(key)
+        print (value)
+
+    return (value)
+
+
+@app.route("/redistest", methods=['GET', 'POST'])
+def redistest():
+    myHostname = "poc-a2subcache.redis.cache.windows.net"
+    myPassword = "NJmqGdGgab4ZVlMcGKPbdF48D9JJilL+tExVYI+PU9Y="
+
+    r = redis.StrictRedis(host=myHostname, port=6380,
+                          password=myPassword, ssl=True)
+
+    result = r.ping()
+    print("Ping returned : " + str(result))
+
+    result = r.set("Message", "Hello!, The cache is working with Python!")
+    print("SET Message returned : " + str(result))
+
+    result = r.get("Message")
+    print("GET Message returned : " + result.decode("utf-8"))
+
+    result = r.client_list()
+    print("CLIENT LIST returned : ")
+    for c in result:
+        print("id : " + c['id'] + ", addr : " + c['addr'])
+
+    return json.dumps(result)
