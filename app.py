c574ed141e48f2630b2331edf1ba94799745d5a9
Julianna Cole <jcole@accme.org>
2021-04-21 10:12:50 -0500

    added wamember and a2queue routes

--- app.py
+++ app.py
@@ -8,6 +8,39 @@ app = Flask(__name__)
 def hello():
    return "Hello World JSON posted"
 
+@app.route("/wamember", methods=['GET', 'POST'])
+def wamember():
+    #set up redis cache access
+    myHostname = "poc-a2subcache.redis.cache.windows.net"
+    myPassword = "NJmqGdGgab4ZVlMcGKPbdF48D9JJilL+tExVYI+PU9Y="
+    r = redis.StrictRedis(host=myHostname, port=6380,
+                          password=myPassword, ssl=True)
+
+    #print (request.is_json)
+    content = request.get_json()
+    result = r.set(content['AccountId'],json.dumps(content))
+    print (content['AccountId'])
+    #print (content)
+    return json.dumps(content)
+    #return ("Hello World - in wamember")
+
+
+@app.route("/a2queue", methods=['GET', 'POST'])
+def a2queue():
+    myHostname = "poc-a2subcache.redis.cache.windows.net"
+    myPassword = "NJmqGdGgab4ZVlMcGKPbdF48D9JJilL+tExVYI+PU9Y="
+
+    r = redis.StrictRedis(host=myHostname, port=6380,
+                          password=myPassword, ssl=True)
+
+    mymap = r.keys(pattern='*')
+    for key in mymap:
+        value = r.get(key)
+        print (value)
+
+    return (value)
+
+
 @app.route("/redistest", methods=['GET', 'POST'])
 def redistest():
     myHostname = "poc-a2subcache.redis.cache.windows.net"
